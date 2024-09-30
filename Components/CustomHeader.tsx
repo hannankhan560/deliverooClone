a28d3edeb469431ca596aa346267e9950624deb3
Hannan Khan <hannan.khan.560@gmail.com>
2024-09-30 11:43:18 -0700

    first commit

---
+++ Components/CustomHeader.tsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import { View, Text, StyleSheet, Image, TouchableOpacity  } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Ionicons } from '@expo/vector-icons';
+import Colors from '@/constants/Colors';
+import { Link } from 'expo-router';
+import { TextInput } from 'react-native-gesture-handler';
+
+
+const SearchBar = () => (
+    <View style={styles.searchContainer}>
+        <View style={styles.searchSection}>
+            <View style={styles.searchField}>
+                <TextInput placeholder='Search' />
+            </View>
+            <Link href={'/'} asChild>
+                <TouchableOpacity style={styles.optionButton}>
+                    <Ionicons name="options-outline" size={20} color={Colors.primary}></Ionicons>
+                </TouchableOpacity>
+            </Link>
+        </View>
+    </View>
+);
+
+const CustomHeader = () => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+        <View style = {styles.container}>
+            <TouchableOpacity >
+                <Image style={styles.bike} source={ require('@/assets/images/bike.png')} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.titleContainer}>
+                <Text style={styles.title}>Delivery · Now</Text>
+                <View style={styles.locationName}>
+                    <Text style={styles.subTitle}>London</Text>
+                    <Ionicons name='chevron-down' size={20} color={Colors.primary} />
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.profileButton}>
+                <Ionicons name='person-outline' size={20} color={Colors.primary} />
+            </TouchableOpacity>
+        </View>
+        <SearchBar />
+    </SafeAreaView>
+  )
+};
+
+const styles = StyleSheet.create ({
+    safeArea: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    container: {
+        height: 60,
+        backgroundColor: '#fff',
+        flexDirection: 'row',
+        gap: 20,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20
+    },
+    bike: {
+        height: 30,
+        width: 30
+    },
+    titleContainer: {
+        flex: 1
+    },
+    title: {
+        fontSize: 14,
+        color: Colors.medium
+    },
+    subTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginRight: 5,
+    },
+    profileButton: {
+        backgroundColor: Colors.lightGrey,
+        padding: 5,
+        borderRadius: 50,
+    },
+    locationName: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    searchContainer :{
+        backgroundColor: "#fff",
+        height: 40,
+        paddingHorizontal: 20,
+        alignItems: 'center',
+        borderRadius: 10
+        
+    },
+    searchSection: {
+        flexDirection: 'row',
+        gap: 20,
+    },
+    searchField: {
+        flex: 1,
+        backgroundColor: Colors.lightGrey,
+        paddingHorizontal: 10
+    },
+    optionButton: {
+        paddingTop: 5,
+    }
+})
+
+export default CustomHeader
---
+++ app/_layout.tsx
@@ -0,0 +1,24 @@
+import {Stack } from 'expo-router';
+import CustomHeader from '../Components/CustomHeader';
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: 'index',
+};
+
+export default function RootLayoutNav() {
+
+  return (
+      <Stack>
+        <Stack.Screen name="index" options={{
+          header: () => <CustomHeader />,
+
+        }} />
+      </Stack>
+  );
+}
